c9b125b486c1cdb12668297a84c5ef4ed1daec6d
K-Sungsil <lovemykth@naver.com>
2024-01-09 15:46:21 +0900

    JDBC 상세조회/등록/수정/삭제 기능구현

--- my-servlet-board/src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
+++ my-servlet-board/src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
@@ -2,10 +2,7 @@ package com.kitri.myservletboard.dao;
 
 import com.kitri.myservletboard.data.Board;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -74,21 +71,127 @@ public class BoardJdbcDao implements BoardDao {
 
     @Override
     public Board getById(Long id) {
-        return null;
+        // connection
+        // ps -> executeQuery();
+        // rs
+
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        Board Board = new Board();
+
+        try {
+            connection =connectDB();
+            String sql = "SELECT * FROM board WHERE id=?";
+
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                String writer = rs.getString("writer");
+                LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+                int viewCount = rs.getInt("view_count");
+                int commentCount = rs.getInt("comment_count");
+                Board board = new Board(id, title, content, writer, createdAt,viewCount, commentCount);
+                return board;
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return Board;
     }
 
     @Override
     public void save(Board board) {
+        Connection conn = null;
+        PreparedStatement ps = null;
 
+        try {
+            conn =connectDB();
+
+            String sql = "INSERT INTO board (title, content, writer) VALUES(?, ?, ?)";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContent());
+            ps.setString(3, board.getWriter());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+
+        }  finally {
+            try {
+                ps.close();
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void update(Board board) {
+        Connection conn = null;
+        PreparedStatement ps = null;
 
+        try {
+            conn =connectDB();
+
+            String sql = "UPDATE board SET title = ?,  content = ? WHERE id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContent());
+            ps.setLong(3, board.getId());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ps.close();
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void delete(Board board) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn =connectDB();
+
+            String sql = "DELETE FROM board WHERE id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setLong(1, board.getId());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ps.close();
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
